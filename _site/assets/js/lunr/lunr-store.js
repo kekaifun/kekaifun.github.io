f7c1ac6e7771bed654245e647fb7ea60847c2df1
kekai <kai.kedal@ele.me>
2019-05-21 15:26:13 +0800

    first post, hello world

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -33,4 +33,9 @@ var store = [{
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
         "tags": ["Jekyll","update"],
         "url": "http://localhost:4000/blog/welcome-to-jekyll/",
+        "teaser":null},{
+        "title": "你好，世界",
+        "excerpt":"这里是我的个人日记，也是2019年我重新出发的起点。   我感觉自己的思绪总是很杂乱，明明还在很认真的考虑一个问题，下一刻脑海里会有不相关的事情不停的冒出来，关键是冒出来的想法有时候还真的很有价值。   关注点只有一个，就像被不同的力量不停的拉扯，思维总是在不停的漂移。最大的问题莫过于不能够集中精力去解决重要的事情。 遇到稍微困难的知识点，需要集中精力去消化的时候， 总会有其他的想法从脑海里冒出来，分散了注意力。困难的知识没有真正的吸收与消化，也就难以得到提高。前进路上最大的阻碍还是自己。   当你以为自己懂了的时候，你真的懂了吗？保持好奇，积极主动 探索，看见，思考，总结，分享   是的，我现在有点乱。每天过着一样的生活，这不是我想要的。   生活和这个世界，可以很美好，也可以很残酷，就看你自己怎么过，从现在开始，我要好好地过自己的生活。 总是对未来抱有期待，却很少认真过好每一天，未来终有一天变为当下。   希望我自己 每一天都有所期待，给未来的自己留一点悬念。   ","categories": ["blog"],
+        "tags": ["思考"],
+        "url": "http://localhost:4000/blog/%E4%BB%8E%E8%AE%B0%E5%BD%95%E5%BC%80%E5%A7%8B/",
         "teaser":null}]
